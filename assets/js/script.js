3a6ad2af85c407a15850ef5f3afddaf6ef9ea585
Lester Jan Gamier <gamier.lester@gmail.com>
2019-06-06 15:01:07 +0800

    includes update for skeleton: nav, and sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 window.onload = () => {
 	var isLoading = true;
+	var links = document.querySelectorAll('.navbar-link');
+	var sections = document.querySelectorAll('section');
+
 	function disableLoader() {
 		document.querySelector('.loader-wrapper').style.display = 'none';
 		document.querySelector('.loader-block').style.width = '0';
@@ -13,6 +16,28 @@ window.onload = () => {
 		return isLoading;
 	}
 
+	function enableSmoothNavigation() {
+		document.querySelectorAll('a[href^="#"]').forEach(element => {
+			element.addEventListener('click', function (event) {
+				event.preventDefault();
+
+				document.querySelector(this.getAttribute('href')).scrollIntoView({
+					behavior: 'smooth'
+				});
+			});
+		});
+	}
+
+	function watchNavStatus() {
+	  let index = sections.length;
+	  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+	  links.forEach((link) => link.classList.remove('active-nav'));
+	  links[index].classList.add('active-nav');
+	}
+
 	if (isLoading) { disableLoader(); }
-	
+	enableSmoothNavigation();
+	watchNavStatus();
+	window.addEventListener('scroll', watchNavStatus);
+
 }
